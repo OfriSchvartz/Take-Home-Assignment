a4b17d9fba888a4f34e7c63f67befc44d9d573fd
Ofri Schvartz <ofrischvartz@gmail.com>
2026-07-16 19:16:10 +0300

    Add feed screen: view/create/delete posts with delete confirmation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react'
 import Login from './Login'
+import Feed from './Feed'
 
 function App() {
   const [token, setToken] = useState(() => sessionStorage.getItem('token'))
+  const [username, setUsername] = useState(() => sessionStorage.getItem('username'))
 
   if (!token) {
     return (
       <Login
-        onLoginSuccess={() => setToken(sessionStorage.getItem('token'))}
+        onLoginSuccess={() => {
+          setToken(sessionStorage.getItem('token'))
+          setUsername(sessionStorage.getItem('username'))
+        }}
       />
     )
   }
 
-  return <div>Logged in! Feed goes here.</div>
+  return <Feed token={token} username={username} />
 }
 
 export default App
